af1be0787945287cfcc14f8425f38d59ef7c97b7
maxzhou1986-debug <maxzhou1986@gmail.com>
2026-09-08 07:32:53 +0800

    Bump fund radar cache to V2.7

--- fund-radar/sw.js
+++ fund-radar/sw.js
@@ -1,4 +1,4 @@
-const CACHE='fund-radar-v2.6';
+const CACHE='fund-radar-v2.7';
 const ASSETS=['./','./index.html','./v2.html','./app-v2.js','./sector-addon.js','./watch-addon.js','./build-addon.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
