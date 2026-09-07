5f8b19432906ed9962e6c4a5abbf8189f5320ace
maxzhou1986-debug <maxzhou1986@gmail.com>
2026-09-07 23:49:05 +0800

    Bump PWA cache for V8 proxy build

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='china-radio-v7';
+const CACHE='china-radio-v8';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
